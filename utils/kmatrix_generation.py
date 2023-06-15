c1876f4e0a376ad1967ba2599a30512b95d08268
MFaizan <engrfaizan.ai@gmail.com>
2023-06-15 20:19:44 +0500

    added utils directory to store all the helper functions

---
+++ utils/kmatrix_generation.py
@@ -0,0 +1,13 @@
+"""
+
+Generate K matrix from the following datasets
+- COLLAB
+- IMDB
+- PROTEINS
+- MUTAG
+
+The K matrix calculates the degree between the different
+nodes in the graph such as source and target node
+"""
+import pandas as pd
+import numpy as np
